16c58824c202d5a49c9a9c6b96e6c5fe06a9fde4
Ivan <reinaldoivan777@gmail.com>
2020-02-24 15:13:06 +0700

    argument type checking on getSortedList function

--- getSortedList.js
+++ getSortedList.js
@@ -1,16 +1,22 @@
 const { compare } = require("./utils/compare");
 const { romanToNumber } = require("./utils/romanToNumber");
 
-const getSortedList = arr => {
-  const sorted = arr
-    .map(name => ({
-      name: name.split(" ")[0],
-      number: romanToNumber(name.split(" ")[1]),
-      romanNumber: name.split(" ")[1]
-    }))
-    .sort((a, b) => compare(a, b))
-    .map(data => `${data.name} ${data.romanNumber}`);
-  return sorted;
+const getSortedList = (arr = []) => {
+  if (Array.isArray(arr)) {
+    try {
+      const sorted = arr
+        .map(name => ({
+          name: name.split(" ")[0],
+          number: romanToNumber(name.split(" ")[1]),
+          romanNumber: name.split(" ")[1]
+        }))
+        .sort((a, b) => compare(a, b))
+        .map(data => `${data.name} ${data.romanNumber}`);
+      return sorted;
+    } catch (error) {
+      throw error;
+    }
+  } else return "Error: Input must be an Array";
 };
 
 module.exports = { getSortedList };
